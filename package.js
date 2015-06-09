526ef208e61a3529021aa974b091c07571cfea11
Paul Bastowski <pbastowski@gmail.com>
2015-06-09 15:45:27 +0200

    Bumped version to 0.0.4. Added github repo address.

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'pbastowski:ft-base',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'FastTrack base packages',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/pbastowski/ft-base',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
