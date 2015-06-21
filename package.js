d0d738ea0c0ca3ce0f5bd6b9cdc384323ef18d3a
Paul Bastowski <pbastowski@gmail.com>
2015-06-21 22:37:28 +0200

    Bumped version to 0.0.5.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'pbastowski:ft-base',
-  version: '0.0.4',
+  version: '0.0.5',
   // Brief, one-line summary of the package.
   summary: 'FastTrack base packages',
   // URL to the Git repository containing the source code for this package.
